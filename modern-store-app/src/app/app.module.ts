49ab362055987d7af242699bb4074e19e2ac2aff
Gabriel Schimidt Mylla <gabrielschimidtmylla@hotmail.com>
2017-07-03 21:49:04 -0300

    prototipo

--- modern-store-app/src/app/app.module.ts
+++ modern-store-app/src/app/app.module.ts
@@ -2,12 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+//Root
 import { AppComponent } from './app.component';
+//shared
+import { HeadbarComponent } from './Components/Shared/headbar/headbar.component';
+import { SubMenuComponent } from './Components/Shared/sub-menu/sub-menu.component';
+import { FooterComponent } from './Components/Shared/footer/footer.component';
+//Compomnents
+import { ProductListComponent } from './Components/product-list/product-list.component';
+//Pages
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { LoginPageComponent } from './Pages/login-page/login-page.component';
+import { SignupPageComponent } from './Pages/signup-page/signup-page.component';
+import { CartPageComponent } from './Pages/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadbarComponent,
+    SubMenuComponent,
+    ProductListComponent,
+    FooterComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
