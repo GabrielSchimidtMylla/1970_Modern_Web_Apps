cb3c7c3cfd9c6fb56aa9e0405009996495102fca
Gabriel Schimidt Mylla <gabrielschimidtmylla@hotmail.com>
2017-07-09 01:34:25 -0300

    Custom Validator

--- modern-store-app/src/app/Pages/login-page/login-page.component.ts
+++ modern-store-app/src/app/Pages/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
+import { CustomValidator } from '../../Validators/custom.validator'
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html'
@@ -9,26 +11,38 @@ export class LoginPageComponent implements OnInit {
 
   public form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { 
+  constructor(private formBuilder: FormBuilder) {
 
     this.form = this.formBuilder.group({
 
       email: ['', Validators.compose([
         Validators.minLength(5),
         Validators.maxLength(160),
-        Validators.required
+        Validators.required,
+        CustomValidator.EmailValidator
       ])],
 
       password: ['', Validators.compose([
         Validators.minLength(6),
         Validators.maxLength(20),
         Validators.required
-      ])]      
-      
+      ])]
+
     });
 
   }
 
   ngOnInit() { }
+
+  checkEmail() {
+    document.getElementById("emailControl").classList.add("is-loading");
+    this.form.controls["email"].disable();
+
+    setTimeout(() => {
+      console.log(this.form.controls["email"].value);
+      this.form.controls["email"].enable();
+      document.getElementById("emailControl").classList.remove("is-loading");
+    }, 3000);
+  }
 
 }
